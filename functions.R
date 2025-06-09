d15c1e5aa0d7037f0c83059f535518e8b0336e5c
Lennart Wittkuhn <lennart.wittkuhn@tutanota.com>
2025-06-09 20:16:18 +0200

    Remove Reading and Survey from schedule

--- functions.R
+++ functions.R
@@ -32,10 +32,10 @@ create_schedule <- function() {
     .[!(title == "") & date_next == 1, title := sprintf("**[%s](%s)**", title, sprintf(session_url, session))] %>%
     .[!(reading == ""), reading := paste("{{< fa book >}}", reading)] %>%
     setnames(.,
-             old = c("session_id", "date", "time", "title", "contents", "reading", "survey"),
-             new = c("No", "Date", "Time", "Title", "Contents", "Reading", "Survey/Quiz")) %>%
-    .[, c("No", "Time", "Title", "Contents", "Reading", "Survey/Quiz")] %>%
-    setcolorder(., c("No", "Time", "Title", "Contents", "Reading", "Survey/Quiz"))
+             old = c("session_id", "date", "time", "title", "contents"),
+             new = c("No", "Date", "Time", "Title", "Contents")) %>%
+    .[, c("No", "Time", "Title", "Contents")] %>%
+    setcolorder(., c("No", "Time", "Title", "Contents"))
   knitr::kable(dt, format = "markdown", align = "l")
 }
 
